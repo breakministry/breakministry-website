682f2000a4009b5234a697bc4bc9f34b047cfaa1
jeffreyb92 <jeffreyb92@gmail.com>
2021-03-24 20:58:30 -0400

    added placeholders for application positions

--- static/js/signup.js
+++ static/js/signup.js
@@ -4,6 +4,9 @@ function onLoad(){
     attr("width", window.innerWidth).
     attr("class", "jesus shadow-lg")
    
+    d3.select("#ministry").style("display","none")
+    d3.select("#creative").style("display","none")
+    d3.select("#tech").style("display","none")
 }
 
 onLoad()
@@ -17,18 +20,44 @@ function handleResize() {
     attr("width", window.innerWidth)
 
 }
-
+// Every time the window size changes, the picture width gets readjusted
 d3.select(window).on("resize", handleResize);
 
-
+// This is set up so that when a person selects which role they want to participate in, the appropriate questions show up
 var option = d3.selectAll("#servicearea");
 option.on("change", updateInterface);
 
 function updateInterface() {
 
-    var picked = d3.selectAll("#servicearea").node().value()
+    //this is the value that is selected from the dropdown and gets updated every time the value is changed
+    var picked = d3.selectAll("#servicearea").node().value
 
     if (picked == 'musicminister') {
-
+//       console.log("YOU PICKED MUSIC MINISTER")
+        d3.select("#music").style("display","block")
+        d3.select("#ministry").style("display","none")
+        d3.select("#creative").style("display","none")
+        d3.select("#tech").style("display","none")
+    }
+    if (picked == 'ministryvolunteer') {
+//       console.log("YOU PICKED MINISTRY VOLUNTEER")
+        d3.select("#music").style("display","none")
+        d3.select("#ministry").style("display","block")
+        d3.select("#creative").style("display","none")
+        d3.select("#tech").style("display","none")
+    }
+    if (picked == 'creativeintern') {
+//        console.log("YOU PICKED CREATIVE INTERN")
+        d3.select("#music").style("display","none")
+        d3.select("#ministry").style("display","none")
+        d3.select("#creative").style("display","block")
+        d3.select("#tech").style("display","none")
+    }
+    if (picked == 'techintern') {
+//        console.log("YOU PICKED TECH INTERN")
+        d3.select("#music").style("display","none")
+        d3.select("#ministry").style("display","none")
+        d3.select("#creative").style("display","none")
+        d3.select("#tech").style("display","block")
     }
 }
